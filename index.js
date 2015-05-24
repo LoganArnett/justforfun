4ab1c23de378796b61dad76638e5d54aa3a6cc21
LoganArnett <Loganarn5@gmail.com>
2015-05-24 13:11:44 -0400

    fun birthday card for Mom

---
+++ index.js
@@ -0,0 +1,7 @@
+$( ".card" ).on("click", function(){
+  $(".card .front").toggleClass("flip");
+  $(".solid").toggleClass("z-up");
+  $('.dancing-taco').toggleClass('animated bounceInDown hidden');
+
+
+});
